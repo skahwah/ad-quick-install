374e0a03bbd901742b829f63d7a5c79cf551db58
Sanjiv Kawa <sanjivkawa@gmail.com>
2021-12-07 11:14:26 -0500

    Update 1-install-adds.ps1
    + Changed DomainMode
    + Changed DomainName 
    + Changed DomainNetbiosName "
    + Changed ForestMode

--- 1-install-adds.ps1
+++ 1-install-adds.ps1
@@ -21,7 +21,7 @@ Import-Module ServerManager
 $password = (ConvertTo-SecureString -AsPlainText -Force -String "Password123")
 
 # install new forest and domain
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2008R2" -DomainName "corp.local" -DomainNetbiosName "CORP" -ForestMode "Win2008R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -SafeModeAdministratorPassword: $password -Force:$true
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode Default -DomainName "lab.local" -DomainNetbiosName "LAB" -ForestMode Default -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -SafeModeAdministratorPassword: $password -Force:$true
 
 # re-enable password complexity
 secedit /export /cfg c:\secpol.cfg
